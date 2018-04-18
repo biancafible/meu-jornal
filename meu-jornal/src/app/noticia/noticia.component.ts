002d395f37360172643caeb5467cc02ffea7b4ce
Your Name <you@example.com>
2018-04-18 14:40:05 +0100

    ativando botões

--- meu-jornal/src/app/noticia/noticia.component.ts
+++ meu-jornal/src/app/noticia/noticia.component.ts
@@ -8,14 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class NoticiaComponent implements OnInit {
 
   noticias: string[] = [
-    "Noticia 1", 
-    "Noticia 2", 
-    "Noticia 3", 
+    "Noticia 1",
+    "Noticia 2",
+    "Noticia 3",
     "Noticia 4"];
-    
+  
+  mostrarNoticia: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public esconderNoticia() {
+     this.mostrarNoticia = false;
   }
 
 }
